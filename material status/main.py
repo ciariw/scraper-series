117265bcfef90a88bdcb47f89cc7ef7e4e69f318
ciariw <90122269+ciariw@users.noreply.github.com>
2022-01-28 16:41:44 -0500

    Update main.py

--- material status/main.py
+++ material status/main.py
@@ -3,20 +3,26 @@ import time
 import openpyxl
 import datetime
 import glob2
+import os
+
 day = datetime.datetime.now().day
 month = datetime.datetime.now().month
 year = datetime.datetime.now().year
+
 if month < 9:
     month = f"0{month}"
 if day < 9:
     day = f"0{day}"
+
 def idworkbook():
     ms = glob2.glob('memstatus/Laminator*.xlsx')
+
     for i in ms:
         booky = openpyxl.load_workbook(i)
         ws = booky["Laminator Membrane Status"]
         nm = ws["A4"].value
-        booky.save(f"memtemp/E{nm[-1]}.xlsx")
+        booky.close()
+        os.rename(i, f"memstatus/E{nm[-1]}.xlsx")
 
 def processing():
     MD = {"E1": {"Lami1": {"Teflon": [[], []], "Membrane": []}, "Lami2": {"Teflon": [[], []], "Membrane": []},
@@ -25,29 +31,31 @@ def processing():
                  "Lami3": {"Teflon": [[], []], "Membrane": []}, "Lami4": {"Teflon": [[], []], "Membrane": []}}
         , "E3": {"Lami1": {"Teflon": [[], []], "Membrane": []}, "Lami2": {"Teflon": [[], []], "Membrane": []},
                  "Lami3": {"Teflon": [[], []], "Membrane": []}, "Lami4": {"Teflon": [[], []], "Membrane": []}}}
-    ms = glob2.glob("memtemp/E*")
+    ms = glob2.glob("memstatus/E*")
     for i in ms:
         book = openpyxl.load_workbook(i)
         ws = book["Laminator Membrane Status"]
+
         for j in list(range(1, 10)):
             # Membrane
-            MD[i[8:10]]["Lami1"]["Membrane"] = [ws["E9"].value, ws["E10"].value]
-            MD[i[8:10]]["Lami2"]["Membrane"] = [ws["E13"].value, ws["E14"].value]
-            MD[i[8:10]]["Lami3"]["Membrane"] = [ws["E17"].value, ws["E18"].value]
-            MD[i[8:10]]["Lami4"]["Membrane"] = [ws["E21"].value, ws["E22"].value]
+            MD[i[10:12]]["Lami1"]["Membrane"] = [ws["E9"].value, ws["E10"].value]
+            MD[i[10:12]]["Lami2"]["Membrane"] = [ws["E13"].value, ws["E14"].value]
+            MD[i[10:12]]["Lami3"]["Membrane"] = [ws["E17"].value, ws["E18"].value]
+            MD[i[10:12]]["Lami4"]["Membrane"] = [ws["E21"].value, ws["E22"].value]
             # Teflons
-            MD[i[8:10]]["Lami1"]["Teflon"][0].append(ws[f"{chr(j + 68)}11"].value)
-            MD[i[8:10]]["Lami1"]["Teflon"][1].append(ws[f"{chr(j + 68)}12"].value)
+            MD[i[10:12]]["Lami1"]["Teflon"][0].append(ws[f"{chr(j + 68)}11"].value)
+            MD[i[10:12]]["Lami1"]["Teflon"][1].append(ws[f"{chr(j + 68)}12"].value)
 
-            MD[i[8:10]]["Lami2"]["Teflon"][0].append(ws[f"{chr(j + 68)}15"].value)
-            MD[i[8:10]]["Lami2"]["Teflon"][1].append(ws[f"{chr(j + 68)}16"].value)
+            MD[i[10:12]]["Lami2"]["Teflon"][0].append(ws[f"{chr(j + 68)}15"].value)
+            MD[i[10:12]]["Lami2"]["Teflon"][1].append(ws[f"{chr(j + 68)}16"].value)
 
-            MD[i[8:10]]["Lami3"]["Teflon"][0].append(ws[f"{chr(j + 68)}19"].value)
-            MD[i[8:10]]["Lami3"]["Teflon"][1].append(ws[f"{chr(j + 68)}20"].value)
+            MD[i[10:12]]["Lami3"]["Teflon"][0].append(ws[f"{chr(j + 68)}19"].value)
+            MD[i[10:12]]["Lami3"]["Teflon"][1].append(ws[f"{chr(j + 68)}20"].value)
 
-            MD[i[8:10]]["Lami4"]["Teflon"][0].append(ws[f"{chr(j + 68)}23"].value)
-            MD[i[8:10]]["Lami4"]["Teflon"][1].append(ws[f"{chr(j + 68)}24"].value)
+            MD[i[10:12]]["Lami4"]["Teflon"][0].append(ws[f"{chr(j + 68)}23"].value)
+            MD[i[10:12]]["Lami4"]["Teflon"][1].append(ws[f"{chr(j + 68)}24"].value)
     return MD
+
 def listify(a):
     # The goal here is to transform the dictionary into a format that i can use to just place on the excel sheet
     # This might kill me
@@ -124,17 +132,18 @@ def save(omega):
 
     wb.save("Laminator_Consumable Parts_Life Span Trend.xlsx")
 
-
 def main():
     save(listify(processing()))
-
-
 
 def run():
     membrane.initialize()
     membrane.download()
     time.sleep(4)
     membrane.driver.quit()
+
+
+
+
 
 if __name__ == '__main__':
     run()
